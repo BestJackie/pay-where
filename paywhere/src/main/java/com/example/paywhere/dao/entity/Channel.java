03b17987e0cdb7fccf50f70a733851df6a3ee8c8
jackieyung <jackieyung@aliyun.com>
2021-09-16 17:20:23 +0800

    新增渠道

--- paywhere/src/main/java/com/example/paywhere/dao/entity/Channel.java
+++ paywhere/src/main/java/com/example/paywhere/dao/entity/Channel.java
@@ -3,15 +3,17 @@ package com.example.paywhere.dao.entity;
 public enum Channel {
     UNIPAY("银联"),
     ALIPAY("支付宝"),
-    WEICHAT("微信");
-    private String displayName;
+    CASH("现金"),
+    CARD("刷卡"),
+    WECHAT("微信");
+    private final String displayName;
 
 
-   Channel(String displayName) {
-       this.displayName=displayName;
-   }
+    Channel(String displayName) {
+        this.displayName = displayName;
+    }
 
-   public String getDisplayName(){
-       return this.displayName;
-   }
+    public String getDisplayName() {
+        return this.displayName;
+    }
 }
