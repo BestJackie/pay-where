e6275fead53baa9cb63beed9e14bf84d276571cb
jackieyung <jackieyung@aliyun.com>
2021-10-22 15:25:30 +0800

    websocket配置

---
+++ paywhere/src/main/java/com/example/paywhere/web/config/WebScoketConfig.java
@@ -0,0 +1,33 @@
+package com.example.paywhere.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * FileName: WebScoketConfig
+ *
+ * @author: admin
+ * Date:     2021-10-22 8:59
+ * Description:
+ * Since: 1.0.0
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebScoketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket").setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic","/user");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+    }
+}
