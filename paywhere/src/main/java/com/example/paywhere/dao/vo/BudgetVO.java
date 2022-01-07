5d77c5980bfa75b0253c9a98201f3344019c9a36
jackieyung <jackieyung@aliyun.com>
2022-01-07 14:50:26 +0800

    fixbug

---
+++ paywhere/src/main/java/com/example/paywhere/dao/vo/BudgetVO.java
@@ -0,0 +1,11 @@
+package com.example.paywhere.dao.vo;/**
+ * FileName: BudgetVO
+ * @author:   admin
+ * Date:     2021-12-21 14:44
+ * Description: 
+ * Since: 1.0.0
+ */
+
+public class BudgetVO {
+   //TODO
+}
---
+++ paywhere/src/main/java/com/example/paywhere/web/controller/BudgetController.java
@@ -0,0 +1,11 @@
+package com.example.paywhere.web.controller;/**
+ * FileName: BudgetController
+ * @author:   admin
+ * Date:     2021-12-21 14:42
+ * Description: 
+ * Since: 1.0.0
+ */
+
+public class BudgetController {
+   //TODO
+}
